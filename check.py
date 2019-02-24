8babb3a9f37ef7b0e134cef5301f288f6a4eb650
Ankit Kumar Namdeo <ankitnamdeo34@gmail.com>
2019-02-24 11:22:14 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,31 +1,40 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
 def find_capital(T):
     for i in range(len(T)):
         if T[i] == i:
             return i
-    
+
 
 def calculate_distance(T, D, i, capital):
     check = i
     total_distance = 0
-    while(1):
-        if D[check]!=0:
+    print "in function"
+    while (1):
+        if D[check] != 0:
+            print "here"
             return total_distance + D[check]
         else:
-            if T[i] == capital:
-                return total_distance
+            print "there"
+            print T[check]
+            if T[check] == capital:
+                return total_distance +1
             else:
-                check = T[i]
-                total_distance += 1 
-        
+                check = T[check]
+                total_distance += 1
+
+
 def solution(T):
     # write your code in Python 3.6
     capital = find_capital(T)
-    distance_matrix = [0]*(len(T))
+    distance_matrix = [0] * (len(T))
+    # distance_matrix[0] = calculate_distance(T, distance_matrix, 0, capital)
     for i in range(len(T)):
         distance_matrix[i] = calculate_distance(T, distance_matrix, i, capital)
-    
-    print (distance_matrix)     
-    result_array = [0]*(len(T)-1)
+
+    print (distance_matrix)
+    result_array = [0] * (len(T))
+
+    for i in range(len(distance_matrix)):
+        result_array[distance_matrix[i]] +=1
+    print (result_array[1:])
+T = [9,1,4,9,0,4,8,9,0,1]
+solution(T)
